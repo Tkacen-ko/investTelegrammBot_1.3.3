d065ce233a7ab7b76e450723e0c10f5f6bc21545
Dmitr Tkachenko <mitja-tkachenk@rambler.ru>
2021-10-01 18:39:36 +0300

    Second commit in the IDE Proba

--- src/main/java/tipa/ExchangeInformationDAO.java
+++ src/main/java/tipa/ExchangeInformationDAO.java
@@ -4,5 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class ExchangeInformationDAO {
+ //sdafsdjkfldsjflk
+    public void yul(){
 
+    }
 }
