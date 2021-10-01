53bd17955087bb0180b7c7a693d09af9f8dc12aa
Dmitr Tkachenko <mitja-tkachenk@rambler.ru>
2021-10-01 20:48:33 +0300

    understand how to implement the Parser class

--- src/main/java/tipa/App.java
+++ src/main/java/tipa/App.java
@@ -3,57 +3,18 @@ package tipa;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 
 public class App {
-    private final JdbcTemplate jdbcTemplate;
-    @Autowired
-    public App(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate = jdbcTemplate;
-    }
-    public static void main(String[] args){
-        Collection<Collection<String>>mass = new ArrayList<>();
-        try {
-            for (int i = 0; i <= 283; i++) {
-                Collection <String> sti = new ArrayList<>();
-                Element e = Parser.getPage().get(i);
-                if (e.select("td").size() == 20) {
-                    Date date=new SimpleDateFormat("dd:MM:yyyy").parse(e.select("td").get(1).text());
-                    jdbcTemplate.update("INSERT INTO smart_lab_db VALUE()");
-                    for (int j = 0; j < 20; j++) {
-
-                        jdbcTemplate.update("INSERT INTO smart_lab_db VALUE ")
-                        System.out.print(e.select("td").get(j).text()+"");
-                    }
-                    System.out.println();
-//                for(Element td : e.select("td")){
-//                    String t =  td.text();
-//                    System.out.println("Сейчас в стринге: "+ t);
-//                }
+    public static void main(String[] args) throws Exception {
 
 
-//                    for(Element td : e.select("td")){
-//                        sti.add(td.text());
-//                    }
-//                    mass.add(sti);
-                }
-            }
-        }
-        catch (Exception e) {
-            System.out.println("!!! \nВнимание я влетел в блок иключения\n");
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException interruptedException) {
-                interruptedException.printStackTrace();
-            }
-        }
-        for (Collection<String>a : mass){
-            System.out.println(a);
-        }
     }
 }
--- src/main/java/tipa/Parser.java
+++ src/main/java/tipa/Parser.java
@@ -4,16 +4,64 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
 
 public class Parser {
-    public static Elements getPage() throws Exception{
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public Parser(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public Integer ingratro(String s) {
+        return Integer.parseInt(s.replaceAll("[%]", ""));
+    }
+    public void getPage() throws Exception{
         String url = "https://smart-lab.ru/q/shares/order_by_issue_capitalization_usd/desc/";
         Document page = Jsoup.parse(new URL(url), 3000);
         Element table = page.select("table[class=simple-little-table trades-table]").first();
         Elements el2 = table.getElementsByTag("tr");
-        return el2;
+        Collection<Collection<String>>mass = new ArrayList<>();
+        try {
+            for (int i = 0; i <= 283; i++) {
+                Element e = el2.get(i);
+                if (e.select("td").size() == 20) {
+                    Date date=new SimpleDateFormat("dd:MM:yyyy").parse(e.select("td").get(1).text());
+
+                    jdbcTemplate.update("INSERT INTO smart_lab_db VALUE(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                            date,
+                            e.select("td").get(2).text(),
+                            e.select("td").get(3).text(),
+                            e.select("td").get(7).text(),
+                            e.select("td").get(14).text(),
+                            e.select("td").get(15).text(),
+                            ingratro(e.select("td").get(8).text()),
+                            ingratro(e.select("td").get(10).text()),
+                            ingratro(e.select("td").get(11).text()),
+                            ingratro(e.select("td").get(13).text())
+                            );
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("!!! \nВнимание я влетел в блок иключения\n");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+        }
     };
 }
 
