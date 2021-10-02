bbbf399fe7ac4a5222400716d4f42b69d92271d2
Dmitr Tkachenko <mitja-tkachenk@rambler.ru>
2021-10-02 23:27:49 +0300

    Loading historical data to DB

--- src/main/java/tipa/App.java
+++ src/main/java/tipa/App.java
@@ -1,7 +1,12 @@
 package tipa;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -11,10 +16,10 @@ import java.util.Date;
 
 public class App {
     public static void main(String[] args) throws Exception {
-//        AnnotationConfigApplicationContext applicationContext =
-//        new AnnotationConfigApplicationContext(SpringConfig.class);
-//        Parser parser = applicationContext.getBean("parser", Parser.class);
-//        parser.getPage();
-    }
+        AnnotationConfigApplicationContext applicationContext =
+        new AnnotationConfigApplicationContext(SpringConfig.class);
+        Parser parser = applicationContext.getBean("parser", Parser.class);
+        parser.getPage();
 
+    }
 }
--- src/main/java/tipa/Parser.java
+++ src/main/java/tipa/Parser.java
@@ -21,22 +21,53 @@ import java.util.Date;
 public class Parser {
     public static final String urdSmarlLabNoDate = "https://smart-lab.ru/q/shares/order_by_issue_capitalization/desc/?date=";
     private static Date dateStart;
-    private static final Date nowDate= new Date();
+    private static String dateDataBD;
+    private static final Date nowDate = new Date();
     static {
         try {
-            dateStart = new SimpleDateFormat( "dd.MM.yyyy" ).parse( "01.01.2015" );
+            dateStart = new SimpleDateFormat("dd.MM.yyyy").parse("01.01.2018");
         } catch (ParseException e) {
             e.printStackTrace();
         }
     }
 
     private final JdbcTemplate jdbcTemplate;
-
     @Autowired
     public Parser(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    public void getPage() throws Exception {
+        while (true) {
+            String url = getUrl();
+            if (url.length() == 0) {
+                break;
+            }
+            Document page = Jsoup.parse(new URL(url), 3000);
+            if (page.select("table[class=simple-little-table trades-table]").first().getElementsByTag("tr").size() == 1) {
+                continue;
+            }
+            Element table = page.select("table[class=simple-little-table trades-table]").first();
+            Elements el2 = table.getElementsByTag("tr");
+            try {
+                for (int i = 1; i <= el2.size()-1; i++) {
+                    Element e = el2.get(i);
+                    if (e.select("td").size() == 18) {
+                        createTable(e, url);
+                    }
+                }
+                System.out.println();
+            } catch (Exception e) {
+                System.out.println("!!! \nВнимание я влетел в блок иключения\n");
+                e.printStackTrace();
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+            }
+        }
+    }
     // метод для чистки значений от лишних знаков
     public Double ingratro(String s) {
         if (s.length() != 0) {
@@ -48,66 +79,49 @@ public class Parser {
 
     // метод предоставляющий ссылку URL для считывания данных
     public String getUrl() {
-        if (dateStart.toString().equals(nowDate.toString())){
+        if (dateStart.toString().equals(nowDate.toString())) {
             return null;
         }
         SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
         String urlForReturn = urdSmarlLabNoDate + formatForDateNow.format(dateStart);
-        System.out.println("Дата до изменения " +dateStart);
         Calendar instance = Calendar.getInstance();
         instance.setTime(dateStart);
         instance.add(Calendar.DAY_OF_MONTH, 1);
+        dateDataBD = formatForDateNow.format(dateStart);
+        System.out.println(dateDataBD);
         dateStart = instance.getTime();
-        System.out.println("Дата до после " +dateStart);
         return urlForReturn;
     }
-
-    public void getPage() throws Exception {
-        while(true){
-        String url = getUrl();
-        if (url.length() == 0) {
-            break;
-        }
-        Document page = Jsoup.parse(new URL(url), 3000);
-        if(page.select("table[class=simple-little-table trades-table]").size()==0){
-            continue;
-        }
-        Element table = page.select("table[class=simple-little-table trades-table]").first();
-        Elements el2 = table.getElementsByTag("tr");
-        Collection<Collection<String>> mass = new ArrayList<>();
+    public void createTable(Element elTd, String url){
         try {
-            for (int i = 0; i <= 283; i++) {
-                Element e = el2.get(i);
-                Date dateNow = new Date();
-                SimpleDateFormat date = new SimpleDateFormat("hh:mm:ss");
-                String ss = date.format(dateNow).toString();
-                if (e.select("td").size() == 18) {
-                    //Date date=new SimpleDateFormat("dd:MM:yyyy").parse(e.select("td").get(1).text());
-                    jdbcTemplate.update("INSERT INTO alldatatiket VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                            i,
-                            e.select("td").get(1).text(),
-                            e.select("td").get(2).text(),
-                            ingratro(e.select("td").get(6).text()),
-                            ingratro(e.select("td").get(13).text()),
-                            ingratro(e.select("td").get(14).text()),
+            jdbcTemplate.execute("create table "+elTd.select("td").get(2).text()+"Ticet (" +
+                    "    dateData data," +
+                    "    name character varying(255)," +
+                    "    tiket character varying(255)," +
+                    "    price numeric," +
+                    "    capitalizationrub numeric," +
+                    "    capitalizationdollars numeric," +
+                    "    changesday numeric," +
+                    "    changesweek numeric," +
+                    "    changesmonth numeric," +
+                    "    changesyear numeric" +
+                    ")");
+        }
+        catch (Exception e){
+                        jdbcTemplate.update("INSERT INTO "+ elTd.select("td").get(2).text()+"Ticet VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                                elTd.select(dateDataBD),
+                                elTd.select("td").get(1).text(),
+                                elTd.select("td").get(2).text(),
+                                ingratro(elTd.select("td").get(6).text()),
+                                ingratro(elTd.select("td").get(13).text()),
+                                ingratro(elTd.select("td").get(14).text()),
 
-                            ingratro(e.select("td").get(7).text()),
-                            ingratro(e.select("td").get(9).text()),
-                            ingratro(e.select("td").get(10).text()),
-                            ingratro(e.select("td").get(12).text())
-                    );
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("!!! \nВнимание я влетел в блок иключения\n");
-            e.printStackTrace();
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException interruptedException) {
-                interruptedException.printStackTrace();
-            }
+                                ingratro(elTd.select("td").get(7).text()),
+                                ingratro(elTd.select("td").get(9).text()),
+                                ingratro(elTd.select("td").get(10).text()),
+                                ingratro(elTd.select("td").get(12).text())
+                        );
         }
     }
-}
 }
 
