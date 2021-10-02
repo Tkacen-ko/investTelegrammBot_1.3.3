66ac0b0a9c496efbe61250827e0dc3ef9ddcf2a6
Dmitr Tkachenko <mitja-tkachenk@rambler.ru>
2021-10-02 14:32:58 +0300

    added a dynamic URL

--- src/main/java/tipa/App.java
+++ src/main/java/tipa/App.java
@@ -1,23 +1,20 @@
 package tipa;
 
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.sql.DataSource;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        AnnotationConfigApplicationContext applicationContext =
-        new AnnotationConfigApplicationContext(SpringConfig.class);
-        Parser parser = applicationContext.getBean("parser", Parser.class);
-        parser.getPage();
-
+//        AnnotationConfigApplicationContext applicationContext =
+//        new AnnotationConfigApplicationContext(SpringConfig.class);
+//        Parser parser = applicationContext.getBean("parser", Parser.class);
+//        parser.getPage();
     }
+
 }
--- src/main/java/tipa/Parser.java
+++ src/main/java/tipa/Parser.java
@@ -10,21 +10,34 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.net.URL;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 
 @ComponentScan("parser")
 public class Parser {
+    public static final String urdSmarlLabNoDate = "https://smart-lab.ru/q/shares/order_by_issue_capitalization/desc/?date=";
+    private static Date dateStart;
+    private static final Date nowDate= new Date();
+    static {
+        try {
+            dateStart = new SimpleDateFormat( "dd.MM.yyyy" ).parse( "01.01.2015" );
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public Parser(JdbcTemplate jdbcTemplate){
+    public Parser(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-
+    // метод для чистки значений от лишних знаков
     public Double ingratro(String s) {
         if (s.length() != 0) {
             System.out.println(s.replaceAll("[% ]", ""));
@@ -32,12 +45,36 @@ public class Parser {
         }
         return 0.000;
     }
-    public void getPage() throws Exception{
-        String url = "https://smart-lab.ru/q/shares/order_by_issue_capitalization/desc/?date=30.09.2021";
+
+    // метод предоставляющий ссылку URL для считывания данных
+    public String getUrl() {
+        if (dateStart.toString().equals(nowDate.toString())){
+            return null;
+        }
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
+        String urlForReturn = urdSmarlLabNoDate + formatForDateNow.format(dateStart);
+        System.out.println("Дата до изменения " +dateStart);
+        Calendar instance = Calendar.getInstance();
+        instance.setTime(dateStart);
+        instance.add(Calendar.DAY_OF_MONTH, 1);
+        dateStart = instance.getTime();
+        System.out.println("Дата до после " +dateStart);
+        return urlForReturn;
+    }
+
+    public void getPage() throws Exception {
+        while(true){
+        String url = getUrl();
+        if (url.length() == 0) {
+            break;
+        }
         Document page = Jsoup.parse(new URL(url), 3000);
+        if(page.select("table[class=simple-little-table trades-table]").size()==0){
+            continue;
+        }
         Element table = page.select("table[class=simple-little-table trades-table]").first();
         Elements el2 = table.getElementsByTag("tr");
-        Collection<Collection<String>>mass = new ArrayList<>();
+        Collection<Collection<String>> mass = new ArrayList<>();
         try {
             for (int i = 0; i <= 283; i++) {
                 Element e = el2.get(i);
@@ -58,11 +95,10 @@ public class Parser {
                             ingratro(e.select("td").get(9).text()),
                             ingratro(e.select("td").get(10).text()),
                             ingratro(e.select("td").get(12).text())
-                            );
+                    );
                 }
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("!!! \nВнимание я влетел в блок иключения\n");
             e.printStackTrace();
             try {
@@ -72,5 +108,6 @@ public class Parser {
             }
         }
     }
+}
 }
 
